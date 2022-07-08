aa3bcde58e4fa873e110950091ed64d76453da72
Sapiens-bru <Sapiens-bru@yandex.ru>
2022-07-08 14:19:24 +0700

    side branch test updated exception handling

--- src/main/java/restServiceOne/HibernateTest1.java
+++ src/main/java/restServiceOne/HibernateTest1.java
@@ -9,24 +9,23 @@ import restServiceOne.hibernate.entity.User;
 
 public class HibernateTest1 {
     public static void main(String[] args) {
-        SessionFactory sessionFactory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(User.class)
-                .buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
+
 
         User usr = new User(1111,"Vladimir","123123",null);
 
-        try {
+        try(SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+            Session session = sessionFactory.getCurrentSession()
+            )
+        {
             session.beginTransaction();
             session.persist(usr);
             session.getTransaction().commit();
-            session.close();
-            sessionFactory.close();
-        }
-        finally {
-            session.close();
-            sessionFactory.close();
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
         }
 
 
